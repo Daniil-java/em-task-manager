03ed7488eb0b362a98543534d04af067928b6c12
ПК <saponembigas@gmail.com>
2024-11-19 00:38:04 +0700

    fix(et-1): GlobalExceptionHandler fix

--- src/main/java/com/em/taskmanager/exceptions/ErrorResponse.java
+++ src/main/java/com/em/taskmanager/exceptions/ErrorResponse.java
@@ -2,16 +2,39 @@ package com.em.taskmanager.exceptions;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
-import lombok.experimental.Accessors;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
 
 @Data
-@Accessors(chain = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse {
-    private String reasonPhrase;
-    private ErrorStatus errorCode;
-    private String message;
-    private String address;
+    private Date timestamp;
     private int status;
-    private long created;
+    private ErrorStatus errorCode;
+    private String reasonPhrase;
+    private String message;
+    private String error;
+    private String path;
+
+    public ErrorResponse(String message, HttpStatusCode status, WebRequest request) {
+        HttpStatus httpStatus = HttpStatus.resolve(status.value());
+        this.timestamp = new Date();
+        this.error = httpStatus.getReasonPhrase();
+        this.message = message;
+        this.status = httpStatus.value();
+        this.path = ((ServletWebRequest) request).getRequest().getRequestURL().toString();
+    }
+
+    public ErrorResponse(ErrorResponseException e, WebRequest request) {
+        this.status = e.getErrorStatus().getHttpStatus().value();
+        this.reasonPhrase = e.getErrorStatus().getHttpStatus().getReasonPhrase();
+        this.message = e.getErrorStatus().getMessage();
+        this.errorCode = e.getErrorStatus();
+        this.path = ((ServletWebRequest) request).getRequest().getRequestURL().toString();
+        this.timestamp = new Date();
+    }
 }
